f0e14a7d6378ac28d122e737b0428876490856bf
Madhuka96 <madhuka.synotec@gmail.com>
2020-05-12 15:10:52 +0530

    #25 Add banner image for view - product page

---
+++ product-type.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+<!-- Mirrored from designarc.biz/demos/wood-workshop/team.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 10 Mar 2020 08:54:12 GMT -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Wood workshop</title>
+    
+    <!--Favicon-->
+    <link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="manifest" href="favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="../../ms-icon-144x144.html">
+    <meta name="theme-color" content="#ffffff">
+
+   <!--    fonts-->
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400,200,500,600,700,600italic,300' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700,300italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=PT+Serif:400,400italic,700' rel='stylesheet' type='text/css'>
+    
+    <link href='https://fonts.googleapis.com/css?family=Alegreya:400,700,700italic,400italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+   
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/strock-icon.css" rel="stylesheet">
+    <!--    owl-carousel-->
+    <link rel="stylesheet" href="vendors/owlcarousel/owl.carousel.css"> 
+    <!--    css-->
+    <link rel="stylesheet" href="css/style.css">
+    
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<!--header-->  
+<?php
+include ("./header.php");
+?>
+
+<!--breadcrumb-->
+<section class="row header-breadcrumb">
+    <div class="container">
+            <div class="row m0 page-cover">
+                <h2 class="page-cover-tittle">Our Team</h2>
+            <ol class="breadcrumb">
+                <li><a href="index.html">Home</a></li>
+                <li class="active">Our Team</li>
+            </ol>
+            </div>
+        </div>
+</section>
+
+<!--team-->
+ <section class="row sectpad team-area">
+    <div class="container">
+        <div class="row our-team dedicated-team">
+            <div class="col-sm-6 col-md-3 team our-teams">
+                <div class="team-images row m0">
+                    <img src="images/project_1/11.jpg" alt="">
+                </div>
+                <ul class="nav social-icons">
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
+                    <li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+                    <li><a href="#"><i class="fa fa-skype"></i></a></li>
+                </ul>
+                <div class="team-content">
+                    <a href="view-product.php"><h4>Rock Leue</h4></a>
+                    <p>Furniture Carpenter</p>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3 team our-teams">
+                <div class="team-images row m0">
+                    <img src="images/project_1/11.jpg" alt="">
+                </div>
+                <ul class="nav social-icons">
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
+                    <li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+                    <li><a href="#"><i class="fa fa-skype"></i></a></li>
+                </ul>
+                <div class="team-content">
+                    <a href="view-product.php"><h4>Rock Leue</h4></a>
+                    <p>Furniture Carpenter</p>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3 team our-teams">
+                <div class="team-images row m0">
+                    <img src="images/project_1/11.jpg" alt="">
+                </div>
+                <ul class="nav social-icons">
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
+                    <li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+                    <li><a href="#"><i class="fa fa-skype"></i></a></li>
+                </ul>
+                <div class="team-content">
+                    <a href="view-product.php"><h4>Rock Leue</h4></a>
+                    <p>Furniture Carpenter</p>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3 team our-teams">
+                <div class="team-images row m0">
+                    <img src="images/project_1/11.jpg" alt="">
+                </div>
+                <ul class="nav social-icons">
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
+                    <li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+                    <li><a href="#"><i class="fa fa-skype"></i></a></li>
+                </ul>
+                <div class="team-content">
+                    <a href="view-product.php"><h4>Rock Leue</h4></a>
+                    <p>Furniture Carpenter</p>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3 team">
+                <div class="team-images row m0">
+                    <img src="images/project_1/11.jpg" alt="">
+                </div>
+                <ul class="nav social-icons">
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
+                    <li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+                    <li><a href="#"><i class="fa fa-skype"></i></a></li>
+                </ul>
+                <div class="team-content">
+                    <a href="view-product.php"><h4>Rock Leue</h4></a>
+                    <p>Furniture Carpenter</p>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3 team">
+                <div class="team-images row m0">
+                    <img src="images/project_1/11.jpg" alt="">
+                </div>
+                <ul class="nav social-icons">
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
+                    <li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+                    <li><a href="#"><i class="fa fa-skype"></i></a></li>
+                </ul>
+                <div class="team-content">
+                    <a href="view-product.php"><h4>Rock Leue</h4></a>
+                    <p>Furniture Carpenter</p>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3 team">
+                <div class="team-images row m0">
+                    <img src="images/project_1/11.jpg" alt="">
+                </div>
+                <ul class="nav social-icons">
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
+                    <li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+                    <li><a href="#"><i class="fa fa-skype"></i></a></li>
+                </ul>
+                <div class="team-content">
+                    <a href="view-product.php"><h4>Rock Leue</h4></a>
+                    <p>Furniture Carpenter</p>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-3 team">
+                <div class="team-images row m0">
+                    <img src="images/project_1/11.jpg" alt="">
+                </div>
+                <ul class="nav social-icons">
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-linkedin-square"></i></a></li>
+                    <li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+                    <li><a href="#"><i class="fa fa-skype"></i></a></li>
+                </ul>
+                <div class="team-content">
+                    <a href="view-product.php"><h4>Rock Leue</h4></a>
+                    <p>Furniture Carpenter</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+<!--great-work-->
+<section class="emergency-contact">
+    <div class="left-side">
+        <div class="content">
+            <h3>If you have any wood work  need...</h3>
+            <h3>Simply call our 24 hour emergecny number.</h3>
+            <a href="tel:+018655248503" class="phone"><img src="images/great-work/phone.png" alt="">01865 524 8503</a>
+            <a href="tel:+018655248503" class="email"><img src="images/great-work/email.png" alt="">info@woodworshop.com</a>
+        </div>
+    </div>
+    <div class="right-side">
+        <img src="images/great-work/right-img.jpg" alt="">
+    </div>
+</section>
+
+<!--footer-->
+<!--footer-->
+<?php
+include ("./footer.php");
+?>
+   
+<script src="js/jquery-2.2.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="vendors/imagesloaded/imagesloaded.pkgd.min.js"></script>
+<script src="vendors/isotope/isotope.min.js"></script>
+<script src="vendors/owlcarousel/owl.carousel.min.js"></script>
+<script src="js/theme.js"></script>
+</body>
+
+<!-- Mirrored from designarc.biz/demos/wood-workshop/team.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 10 Mar 2020 08:54:12 GMT -->
+</html>
