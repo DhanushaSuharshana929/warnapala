173f243ec0a2c3fbf72d029ae1a70cec21891fcd
Madhuka96 <madhuka.synotec@gmail.com>
2020-05-12 11:33:02 +0530

    #23 add slider image for view-product page

---
+++ view-product.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+<!-- Mirrored from designarc.biz/demos/wood-workshop/blog-details.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 10 Mar 2020 08:54:15 GMT -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Wood workshop</title>
+    
+    <!--Favicon-->
+    <link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="manifest" href="favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="../../ms-icon-144x144.html">
+    <meta name="theme-color" content="#ffffff">
+
+   <!--    fonts-->
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400,200,500,600,700,600italic,300' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700,300italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=PT+Serif:400,400italic,700' rel='stylesheet' type='text/css'>
+    
+    <link href='https://fonts.googleapis.com/css?family=Alegreya:400,700,700italic,400italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+   
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/strock-icon.css" rel="stylesheet">
+    <!--    owl-carousel-->
+    <link rel="stylesheet" href="vendors/owlcarousel/owl.carousel.css"> 
+    <!--    css-->
+    <link rel="stylesheet" href="css/style.css">
+    
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<!--header-->  
+<?php
+include ("./header.php");
+?>
+
+<!--breadcrumb-->
+<section class="row header-breadcrumb">
+    <div class="container">
+        <div class="row m0 page-cover">
+            <h2 class="page-cover-tittle">Blog details</h2>
+            <ol class="breadcrumb">
+                <li><a href="index.html">Home</a></li>
+                <li class="active">Blog details</li>
+            </ol>
+        </div>
+    </div>
+</section>
+
+
+<!--blog-details-->
+<section class="row blog_content">
+    <div class="container">
+        <div class="row sectpad">
+            <div class="blog_section col-lg-8">
+               <div class="row blog blog-details">
+                    <div class="featured_img row m0">
+                   <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <!--img src="la.jpg" alt="Los Angeles" style="width:100%;"-->
+        <img src="images/projects-description/1.jpg" alt="" class="img-responsive">
+      </div>
+
+      <div class="item">
+        <!--img src="chicago.jpg" alt="Chicago" style="width:100%;"-->
+        <img src="images/projects-description/1.jpg" alt="" class="img-responsive">
+      </div>
+    
+      <div class="item">
+        <!--img src="ny.jpg" alt="New york" style="width:100%;"-->
+        <img src="images/projects-description/1.jpg" alt="" class="img-responsive">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+               
+                    </div>
+                    <div class="post-contents row m0">
+                        <div class="row m0 section_header project-top">
+                   <h2>project Description</h2>
+                   <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantitum dolore mque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dict eaque ipsa quae ab illo inventore veritatis et quasi architecto. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. </p>                 
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p> 
+                </div>
+                   </div>
+                </div> <!--Blog-->
+            </div>
+            
+            <div class="sidebar_section col-lg-4">
+                <div class="sidebar row m0 ">
+                    <div class="row widget widget-search">
+                        <div class="row widget-inner">
+                            <form action="#" class="search-form" method="get">
+                                <div class="input-group">
+                                    <input type="search" class="form-control" placeholder="Enter Search keywords">
+                                    <span class="input-group-addon">
+                                        <button type="submit"><i class="icon icon-Search"></i></button>
+                                    </span>
+                                </div>
+                            </form>
+                        </div>
+                    </div> <!--Search-->
+                    <div class="row widget widget-categories">
+                        <h4 class="widget-title">Categories</h4>
+                        <div class="row widget-inner">
+                            <ul class="nav categories">
+                                <li><a href="#"><i class="fa fa-angle-right"></i>Interior Furniture Manfacturing</a></li>
+                                <li><a href="#"><i class="fa fa-angle-right"></i>Reparing of Wooden Almerah</a></li>
+                                <li><a href="#"><i class="fa fa-angle-right"></i>Hardwood Flooring</a></li>
+                                <li><a href="#"><i class="fa fa-angle-right"></i>Renovaion of office furnitures</a></li>
+                                <li><a href="#"><i class="fa fa-angle-right"></i>Make Quality Products</a></li>
+                                <li><a href="#"><i class="fa fa-angle-right"></i>Renovation of Kitchen</a></li>
+                            </ul>
+                        </div>
+                    </div> <!--Categories-->
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--great-work-->
+<section class="emergency-contact">
+    <div class="left-side">
+        <div class="content">
+            <h3>If you have any wood work  need...</h3>
+            <h3>Simply call our 24 hour emergecny number.</h3>
+            <a href="tel:+018655248503" class="phone"><img src="images/great-work/phone.png" alt="">01865 524 8503</a>
+            <a href="tel:+018655248503" class="email"><img src="images/great-work/email.png" alt="">info@woodworshop.com</a>
+        </div>
+    </div>
+    <div class="right-side">
+        <img src="images/great-work/right-img.jpg" alt="">
+    </div>
+</section>
+
+<!--footer-->
+<?php
+include ("./footer.php");
+?>
+   
+<script src="js/jquery-2.2.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="vendors/imagesloaded/imagesloaded.pkgd.min.js"></script>
+<script src="vendors/isotope/isotope.min.js"></script>
+<script src="vendors/owlcarousel/owl.carousel.min.js"></script>
+<script src="js/theme.js"></script>
+</body>
+
+<!-- Mirrored from designarc.biz/demos/wood-workshop/blog-details.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 10 Mar 2020 08:54:19 GMT -->
+</html>
